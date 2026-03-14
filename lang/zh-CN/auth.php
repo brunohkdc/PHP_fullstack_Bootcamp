04e18572f32592745add8e3b94b3d0bbf94d1a15
Career Incubation <155411920+careerincubation@users.noreply.github.com>
2026-03-14 18:41:33 +0800

    Added localization files
    Added localization files for zh-CN demo

---
+++ lang/zh-CN/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '这些凭证与我们的记录不符，或者您的帐户已被禁用。',
+    'password' => '提供的密码不正确。',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
